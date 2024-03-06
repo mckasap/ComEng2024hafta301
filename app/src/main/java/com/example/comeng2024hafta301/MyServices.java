ce9d12754b163484febabb6d117a6f1f64db8b45
mckasapbasi <mckasap@gmail.com>
2024-03-06 14:32:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/comeng2024hafta301/MyServices.java
@@ -0,0 +1,38 @@
+package com.example.comeng2024hafta301;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MyServices extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+
+        Toast.makeText(this,
+                "SERVICE Destroyed",
+                Toast.LENGTH_LONG).show();
+
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Toast.makeText(this,
+                "SERVICE Started",
+                Toast.LENGTH_LONG).show();
+
+
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
